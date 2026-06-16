1799d4892bd1f0e807c9a56efbfcfc384995aca1
Laszlo <laszlo.prekop@gmail.com>
2026-06-16 20:05:46 +0200

    prep: change Movies type to List

--- MovieApi/Models/Actor.cs
+++ MovieApi/Models/Actor.cs
@@ -6,6 +6,6 @@
         public required string Name { get; set; }
         public int BirthYear { get; set; }
         // Mirror collection for the N:M relationship with Movie
-        public ICollection<Movie> Movies { get; set; } = new List<Movie>();
+        public List<Movie> Movies { get; set; } = new();
     }
 }
